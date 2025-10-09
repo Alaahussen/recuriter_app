a563765af70e9800902039caeca938f62c7eb5f7
Alaa hussien <95131001+Alaahussen@users.noreply.github.com>
2025-10-09 03:14:26 +0300

    Update app.py

--- recuiter_project_app/app.py
+++ recuiter_project_app/app.py
@@ -1,6 +1,6 @@
 # streamlit_app.py
 __import__('pysqlite3')
-import sys
+import s
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
@@ -1128,6 +1128,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
